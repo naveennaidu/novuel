3882801dea5196c8293aea71b55718accb124a66
Naveen <mnaveennaidu9927@gmail.com>
2023-09-06 17:24:58 +0530

    lp update

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,27 @@
 <template>
   <div class="mt-16 mx-auto max-w-xl text-center">
     <h1 class="text-5xl font-semibold">Novuel</h1>
+    <p class="mt-2">Open Source AI Writing app</p>
 
-    <UButton @click="continueWithGoogle" class="mt-8">
+    <UButton
+      @click="continueWithGoogle"
+      color="gray"
+      variant="solid"
+      class="mt-8"
+      size="xl"
+    >
+      <IconGoogle class="w-5 h-5" />
       Continue with Google
     </UButton>
+
+    <div class="absolute bottom-12 left-1/2 transform -translate-x-1/2">
+      Built with
+      <div class="flex items-center justify-center gap-x-1">
+        <UButton variant="link">Nuxt 3</UButton>
+        <UButton variant="link">Nuxt UI</UButton>
+        <UButton variant="link">Novel Vue</UButton>
+      </div>
+    </div>
   </div>
 </template>
 
